f77b37535be6cb925eaf8ae0bd90e4945e1674ae
nbfaller <nbfaller@up.edu.ph>
2024-05-12 23:12:30 +0800

    aws fix 4

--- apps/home.py
+++ apps/home.py
@@ -7,7 +7,7 @@ from dash.exceptions import PreventUpdate
 # Other libraries
 import hashlib
 # App definition
-from application import application as app
+from application import app
 from apps import dbconnect as db
 
 # Default margins and spacing settings
