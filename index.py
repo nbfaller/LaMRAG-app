1f6378fe5e6ba48affefd7b8a18dfa297ea85bdc
nbfaller <nbfaller@up.edu.ph>
2024-05-12 23:28:26 +0800

    app.run_server replaced with app.run

--- index.py
+++ index.py
@@ -114,4 +114,4 @@ def displaypage(pathname, sessionlogout, user_id, usertype_id):
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:8050', new = 0, autoraise = True)
-    app.run_server(debug = False)
+    app.run(debug = False)
